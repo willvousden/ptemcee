cdc2cdb4bc5df866fcaf8dea91049031631d9e74
Will Vousden <will@vousden.me.uk>
2015-05-19 09:09:09 +0100

    Added missing import rules to __init__.

--- ptemcee/__init__.py
+++ ptemcee/__init__.py
@@ -4,6 +4,8 @@
 from __future__ import (division, print_function, absolute_import, unicode_literals)
 
 from .sampler import *
+from .interruptible_pool import InterruptiblePool
+from .mpi_pool import MPIPool
 from . import autocorr
 
 __version__ = '0.9.0'
