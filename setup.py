ec4cdced859579019d0886a275543ccbb577d4f2
Will Vousden <will@vousden.me.uk>
2017-12-05 11:41:02 +0000

    Added Pipfiles.

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     package_data={'': ['LICENSE']},
     include_package_data=True,
     install_requires=['numpy'],
+    tests_require=['nose'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
